112721f57c3e37ac1895f852bd59d4bb05bfb047
lopidav <lopidav@github.com>
2021-08-17 03:24:16 +0300

    wga 1.0.0.11

--- wga/script.js
+++ wga/script.js
@@ -165,14 +165,54 @@ interact('.dice')
     }
   })
 
+interact('.resizable-horisontal')
+	.resizable({
+	edges: { right: true},
+
+	listeners: {
+	  move (event) {
+		var target = event.target
+		var x = (parseFloat(target.getAttribute('data-x')) || 0)
+		var y = (parseFloat(target.getAttribute('data-y')) || 0)
+
+		// update the element's style
+		target.style.width = event.rect.width + 'px'
+		target.style.height = event.rect.height + 'px'
+
+		// translate when resizing from top or left edges
+		x += event.deltaRect.left
+		y += event.deltaRect.top
+
+		target.style.transform = 'translate(' + x + 'px,' + y + 'px)'
+
+		target.setAttribute('data-x', x)
+		target.setAttribute('data-y', y)
+	  }
+	},
+	modifiers: [
+	  // minimum size
+	  interact.modifiers.restrictSize({
+		min: { width: 5, height: 5 }
+	  })
+	],
+
+	inertia: true
+  })
+
 interact('.dice')
   .on('hold', function (event) {
+	var tmp = event.currentTarget.innerText;
     event.currentTarget.innerText = ``;
-    $(event.currentTarget).append($(`<input type="text" style="
+    $(event.currentTarget).append($(`<input class="resizable-horisontal" type="text" style="
     width: 15px;
-">`))
+" value="${tmp}">`))
+	
+	
+	
     event.preventDefault()
   })
+
+
 
 
 // target elements with the "draggable" class
