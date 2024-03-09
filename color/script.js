f6522a5fd4b010c7ca5e87d246ed4e9b0bbe5ffa
lopidav <poluhovichvadim@gmail.com>
2024-03-09 20:06:32 +0200

    added more lvls

--- color/script.js
+++ color/script.js
@@ -12,6 +12,7 @@ $( document ).ready(function() {
 	Mygame.score = 5;
 	Mygame.lvl = 0;
 	Mygame.combo = 0;
+	Mygame.back = $('#back');
 	function hideAll() {
 		Mygame.straigt.hide();
 		Mygame.swapped.hide();
@@ -51,7 +52,7 @@ $( document ).ready(function() {
 				history.pushState({colour:"blue"},"true","?color=blue");
 				break;
 			case "green":
-				Mygame.chosenColor = [0,200,0];
+				Mygame.chosenColor = [0,150,0];
 				history.pushState({colour:"green"},"true","?color=green");
 				break;
 			case "yellow":
@@ -70,29 +71,79 @@ $( document ).ready(function() {
 	function Setup() {
 		$('.left').css('background', `rgb(${Mygame.chosenColor})`);
 		
-		if (Mygame.lvl < 42) {
-			setWrongColorForLvl(Mygame.lvl);
-			$('.right').css("border", `none`);
-			$('.left').css("border", `none`);
-		}
-		else if (Mygame.lvl < 55) {
-			setWrongColorForLvl(Mygame.lvl - 13);
-			$('.right').css("border", `solid 1px rgb(${ [(a=Mygame.chosenColor.reduce((y,x)=>y+x)/2.3|0),a,a].join`,` }`)
-			$('.left').css("border", `solid 1px rgb(${ [(a=Mygame.chosenColor.reduce((y,x)=>y+x)/2.3|0),a,a].join`,` }`)
-		}
-		else if (Mygame.lvl < 68) {
-			setWrongColorForLvl(Mygame.lvl - 26);
-			$('.right').css("border", `solid 10px rgb(${ [(a=Mygame.chosenColor.reduce((y,x)=>y+x)/2.3|0),a,a].join`,` }`)
-			$('.left').css("border", `solid 10px rgb(${ [(a=Mygame.chosenColor.reduce((y,x)=>y+x)/2.3|0),a,a].join`,` }`)
-		}
-		else if (Mygame.lvl < 81) {
-			setWrongColorForLvl(Mygame.lvl - 39);
-			$('.right').css("border", `solid 50px rgb(${ [(a=Mygame.chosenColor.reduce((y,x)=>y+x)/2.3|0),a,a].join`,` }`)
-			$('.left').css("border", `solid 50px rgb(${ [(a=Mygame.chosenColor.reduce((y,x)=>y+x)/2.3|0),a,a].join`,` }`)
-		}
-		else {
-		}
+		setWrongColorForLvl(getColorLvl(Mygame.lvl));
+		setBorderForLvl(getBorderLvl(Mygame.lvl));
+		setFlickeringForLvl(Mygame.lvl);
 		// Mygame.loose.css('background', `rgb(${ [(a=Mygame.chosenColor.reduce((y,x)=>y+x)|0),a,a].join`,` })`)
+	}
+	function getBorderLvl(lvl) {
+		if (lvl < 133) return  lvl;
+		return lvl - 113;
+	}
+	function setBorderForLvl(lvl)
+	{
+		if (lvl < 42) {
+			// $('.right').css("border", `none`);
+			// $('.left').css("border", `none`);
+		}
+		else if (lvl < 55) {
+			$('.right').css("width", "calc(100% - 1px)");
+			$('.right').css("height", "100%");
+			$('.left').css("width", "calc(100% - 1px)");
+			$('.left').css("height", "100%");
+			// $('.right').css("border", `solid 1px rgb(${ [(a=Mygame.chosenColor.reduce((y,x)=>y+x)/2.3|0),a,a].join`,` }`)
+			// $('.left').css("border", `solid 1px rgb(${ [(a=Mygame.chosenColor.reduce((y,x)=>y+x)/2.3|0),a,a].join`,` }`)
+		}
+		else if (lvl < 68) {
+			$('.right').css("width", "calc(100% - 10px)");
+			$('.right').css("height", "100%");
+			$('.left').css("width", "calc(100% - 10px)");
+			$('.left').css("height", "100%");
+			// $('.right').css("border", `solid 10px rgb(${ [(a=Mygame.chosenColor.reduce((y,x)=>y+x)/2.3|0),a,a].join`,` }`)
+			// $('.left').css("border", `solid 10px rgb(${ [(a=Mygame.chosenColor.reduce((y,x)=>y+x)/2.3|0),a,a].join`,` }`)
+		}
+		else if (lvl < 81) {
+			$('.right').css("width", "calc(90%)");
+			$('.right').css("height", "calc(90%)");
+			$('.left').css("width", "calc(90%)");
+			$('.left').css("height", "calc(90%)");
+			// $('.right').css("border", `solid 50px rgb(${ [(a=Mygame.chosenColor.reduce((y,x)=>y+x)/2.3|0),a,a].join`,` }`)
+			// $('.left').css("border", `solid 50px rgb(${ [(a=Mygame.chosenColor.reduce((y,x)=>y+x)/2.3|0),a,a].join`,` }`)
+			Mygame.win.css('background', `color-mix(in oklab, rgb(${Mygame.chosenColor}) 90%, rgb(50,50,50)`)
+		}
+		else if (lvl < 94) {
+			$('.right').css("width", "calc(70%)");
+			$('.right').css("height", "calc(70%)");
+			$('.left').css("width", "calc(70%)");
+			$('.left').css("height", "calc(70%)");
+			// $('.right').css("border", `solid 10vw rgb(${ [(a=Mygame.chosenColor.reduce((y,x)=>y+x)/2.3|0),a,a].join`,` }`)
+			// $('.left').css("border", `solid 10vw rgb(${ [(a=Mygame.chosenColor.reduce((y,x)=>y+x)/2.3|0),a,a].join`,` }`)
+			Mygame.win.css('background', `color-mix(in oklab, rgb(${Mygame.chosenColor}), rgb(50,50,50)`)
+		}
+		else if (lvl < 107) {
+			$('.right').css("width", "calc(30%)");
+			$('.right').css("height", "calc(20%)");
+			$('.left').css("width", "calc(30%)");
+			$('.left').css("height", "calc(20%)");
+			// $('.right').css("border", `solid 20vw rgb(${ [(a=Mygame.chosenColor.reduce((y,x)=>y+x)/2.3|0),a,a].join`,` }`)
+			// $('.left').css("border", `solid 20vw rgb(${ [(a=Mygame.chosenColor.reduce((y,x)=>y+x)/2.3|0),a,a].join`,` }`)
+			Mygame.win.css('background', `color-mix(in oklab, rgb(${Mygame.chosenColor}), rgb(50,50,50)`)
+		}
+		else if (lvl < 120) {
+			$('.right').css("width", "calc(10%)");
+			$('.right').css("height", "calc(1px)");
+			$('.left').css("width", "calc(10%)");
+			$('.left').css("height", "calc(1px)");
+			// $('.right').css("border", `solid 20vw rgb(${ [(a=Mygame.chosenColor.reduce((y,x)=>y+x)/2.3|0),a,a].join`,` }`)
+			// $('.left').css("border", `solid 20vw rgb(${ [(a=Mygame.chosenColor.reduce((y,x)=>y+x)/2.3|0),a,a].join`,` }`)
+			Mygame.win.css('background', `color-mix(in oklab, rgb(${Mygame.chosenColor}), rgb(50,50,50)`)
+		}
+	}
+	function getColorLvl(lvl)
+	{
+		if (lvl < 42) return  lvl;
+		if (lvl < 133) return  (lvl - 42)%13 + 42-13;
+		return getColorLvl(lvl - 100)
 	}
 	function setWrongColorForLvl(lvl)
 	{
@@ -117,8 +168,51 @@ $( document ).ready(function() {
 			Mygame.difference = 1 + " (Red)";
 			Mygame.win.css('background', `color-mix(in oklab, ${`rgb(${Mygame.chosenColor[0]+1},${Mygame.chosenColor[1]},${Mygame.chosenColor[2]})`}, ${`rgb(${Mygame.chosenColor})`}`)
 		}
+		Mygame.back.css('background', `color-mix(in oklab, rgb(${Mygame.chosenColor}), rgb(50,50,50)`);
 	}
-	$('.left').click(function(){
+	function setFlickeringForLvl(lvl)
+	{
+		if (lvl < 133) {
+			$('.first').css('animation', ``);
+			$('.first').css('-webkit-animation', ``);
+			$('.first').css('-moz-animation', ``);
+			$('.first').css('-o-animation', ``);
+			$('.last').css('animation', ``);
+			$('.last').css('-webkit-animation', ``);
+			$('.last').css('-moz-animation', ``);
+			$('.last').css('-o-animation', ``);
+			$('.last').css('opacity', "1");
+		} else if (lvl < 142) {
+			$('.first').css('animation', `flickerAnimation 3s infinite`);
+			$('.first').css('-webkit-animation', `flickerAnimation 3s infinite`);
+			$('.first').css('-moz-animation', `flickerAnimation 3s infinite`);
+			$('.first').css('-o-animation', `flickerAnimation 3s infinite`);
+			$('.last').css('animation', `reverseFlickerAnimation 3s infinite`);
+			$('.last').css('-webkit-animation', `reverseFlickerAnimation 3s infinite`);
+			$('.last').css('-moz-animation', `reverseFlickerAnimation 3s infinite`);
+			$('.last').css('-o-animation', `reverseFlickerAnimation 3s infinite`);
+			$('.last').css('opacity', "1");
+		} else  if (lvl < 155) {
+			$('.first').css('animation', `appearAnimation 1s forwards`);
+			$('.last').css('animation', `disappearAnimation 0.1s forwards`);
+			$('.last').css('opacity', "0");
+		} else if (lvl < 168) {
+			$('.first').css('animation', `appearDisappearAnimation 1.2s forwards`);
+			$('.last').css('animation', `disappearAnimation 0.1s forwards`);
+			$('.last').css('opacity', "0");
+		} else  if (lvl < 181){
+			$('.first').css('animation', `appearDisappear1pcAnimation 1s forwards`);
+			$('.last').css('animation', `disappearAnimation 0.1s forwards`);
+			$('.last').css('opacity', "0");
+		} else {
+			$('.first').css('animation', `appearDisappear1pxAnimation 1s forwards`);
+			$('.last').css('animation', `disappearAnimation 0.1s forwards`);
+			$('.last').css('opacity', "0");
+		}
+	}
+
+
+	$('.leftBorder').click(function(){
 		hideAll();
 		Mygame.win.css('display', 'flex');
 		Mygame.scorePanel.css('display', 'flex');
@@ -131,7 +225,7 @@ $( document ).ready(function() {
 		if (Mygame.score > 10) {Mygame.score = 5; Mygame.lvl++;Setup()}
 		console.log(Mygame.score, Mygame.lvl)
 	});
-	$('.right').click(function(){
+	$('.rightBorder').click(function(){
 		hideAll();
 		Mygame.loose.css('display', 'flex')
 		Mygame.scorePanel.css('display', 'flex');
