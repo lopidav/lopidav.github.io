3f54969a4e79e49d33d2b310fe5be6be4352da0b
lopidav <poluhovichvadim@gmail.com>
2025-01-06 04:08:33 +0200

    doing more stuff

--- LopisPerkourLeaderboards/script.js
+++ LopisPerkourLeaderboards/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", onStart);
 var pubTsvURL = `https://docs.google.com/spreadsheets/d/e/2PACX-1vTwPlhqxhy5MsAAMs38LOe1jMcayb4VpZkIYKZ1sdltek4PnbEWT7r5AsbuzLZYb0Wd4iXd1_gnEALf/pub?gid=1664204216&single=true&output=tsv`;
 var leaderboards = [];
+
 class Record {
   constructor(dateString, steamId, steamName, map, timeString) {
     this.date = new Date(dateString);
@@ -10,12 +11,23 @@ class Record {
     this.time = +timeString.replace(/[.,]/g,"");
   }
 };
+
 httpGetAsync(pubTsvURL, responce => {
   leaderboards = responce.split("\r\n").map(x=>x.split("\t")).slice(1).map(x=>new Record(x[0],x[1],x[2],x[3],x[4]));
-
+  leaderboards.sortLeaderboardsBy();
   console.log(leaderboards);
   displayLeaderboards()
 });
+
+function sortLeaderboardsBy(byWhat) {
+  switch(byWhat){
+    case "date":
+    default:
+      leaderboards.sort((x,y)=>x.date-y.date)
+      break;
+  }
+}
+
 function displayLeaderboards() {
   document.getElementById("mainTable").innerHTML = leaderboards.map(x=>`
   <tr>
@@ -26,8 +38,8 @@ function displayLeaderboards() {
   </tr>`).join`
 `;
 }
-function onStart()
-{
+
+function onStart() {
   displayLeaderboards();
 }
 function httpGetAsync(theUrl, callback) //https://stackoverflow.com/questions/247483/http-get-request-in-javascript
