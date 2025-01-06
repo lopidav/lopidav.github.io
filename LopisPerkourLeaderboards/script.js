ca4fa76e74664acfcabbd9ac35e47106e1c86a98
lopidav <poluhovichvadim@gmail.com>
2025-01-06 03:44:33 +0200

    some progress

--- LopisPerkourLeaderboards/script.js
+++ LopisPerkourLeaderboards/script.js
@@ -1,12 +1,33 @@
 document.addEventListener("DOMContentLoaded", onStart);
 var pubTsvURL = `https://docs.google.com/spreadsheets/d/e/2PACX-1vTwPlhqxhy5MsAAMs38LOe1jMcayb4VpZkIYKZ1sdltek4PnbEWT7r5AsbuzLZYb0Wd4iXd1_gnEALf/pub?gid=1664204216&single=true&output=tsv`;
 var leaderboards = [];
+class Record {
+  constructor(dateString, steamId, steamName, map, timeString) {
+    this.date = Date.parse(dateString);
+    this.steamId = steamId;
+    this.steamName = steamName;
+    this.map = map;
+    this.time = +timeString.replace(/[.,]/g,"");
+  }
+};
+httpGetAsync(pubTsvURL, responce => {
+  leaderboards = responce.split("\r\n").map(x=>x.split("\t")).slice(1).map(x=>new Record(x[0],x[1],x[2],x[3],x[4]));
+
+  console.log(leaderboards);
+  displayLeaderboards()
+});
+function displayLeaderboards() {
+  document.getElementById("mainTable").innerHTML = leaderboards.map(x=>`
+    <tr>
+     <th>${x.date.toLocaleString()}</th>
+     <th>${x.steamName}</th>
+     <th>${x.map}</th>
+     <th>${x.time}</th>
+    </tr>`);
+}
 function onStart()
 {
-  httpGetAsync(pubTsvURL, responce => {
-    leaderboards = responce.split("\n").map(x=>x.split("\t"));
-    console.log(leaderboards);
-  });
+  displayLeaderboards();
 }
 function httpGetAsync(theUrl, callback) //https://stackoverflow.com/questions/247483/http-get-request-in-javascript
 {
