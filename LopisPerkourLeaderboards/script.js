ad3ae5e53b5aef096ac1ece2a41b192f19bb7d45
lopidav <poluhovichvadim@gmail.com>
2025-01-06 03:18:40 +0200

    parkourleaderboards

---
+++ LopisPerkourLeaderboards/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", onStart);
+var pubTsvURL = `https://docs.google.com/spreadsheets/d/e/2PACX-1vTwPlhqxhy5MsAAMs38LOe1jMcayb4VpZkIYKZ1sdltek4PnbEWT7r5AsbuzLZYb0Wd4iXd1_gnEALf/pub?gid=1664204216&single=true&output=tsv`;
+var leaderboards = [];
+function onStart()
+{
+  httpGetAsync(pubTsvURL, responce => {
+    leaderboards = responce.split("\n").map(x=>x.split("\t"));
+    console.log(leaderboards);
+  });
+}
+function httpGetAsync(theUrl, callback) //https://stackoverflow.com/questions/247483/http-get-request-in-javascript
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.setRequestHeader("Origin", )
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
